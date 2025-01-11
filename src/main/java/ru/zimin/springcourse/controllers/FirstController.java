0068aad764e922baf24fce6bd5d5de541e05c2bc
plaffyyy <k.zimin@innopolis.university>
2025-01-11 12:10:59 +0300

    feat(java config): get with parameters

--- src/main/java/ru/zimin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/zimin/springcourse/controllers/FirstController.java
@@ -1,14 +1,27 @@
 package ru.zimin.springcourse.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class FirstController {
-
+    @GetMapping("/")
+    public String firstPage() {
+        return "first/first";
+    }
     @GetMapping("/hi")
-    public String helloPage() {
+    public String helloPage(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "surname", required = false) String surname,
+            Model model
+    ) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
     @GetMapping("/bye")
